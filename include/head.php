db4ab68eb99416d0be3c7d96d5ced5636150d404
titadone <bloum93200@gmail.com>
2015-12-03 22:52:45 +0100

    head finaliser

--- include/head.php
+++ include/head.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <meta lang="fr">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
+
     <link rel="icon" href="favicon.ico">
 	<title>Nuit de l'info</title>
+    <!--CSS-->
+    <link rel="stylesheet" href="./assets/style.css">
     <link rel="stylesheet" href="./assets/bootstrap/dist/css/bootstrap.min.css">
-	  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	  <script type="text/javascript" src="./assets/js/main.js"></script>
-	  <!-- Include all compiled plugins (below), or include individual files as needed -->
-	  <script src="./assets/bootstrap/js/bootstrap.min.js"></script>
-
-      <link href="./assets/style.css" rel="stylesheet">
-      <audio src="/audio/Pouvoir.mp3"></audio>
+    <!--JAVASCRIPT-->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script type="text/javascript" src="./assets/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="./assets/js/main.js"></script>
+    <!--AUDIO-->
+    <audio src="/audio/Pouvoir.mp3"></audio>
   </head>
