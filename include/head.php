31c36f45b2ee343ad540760d8e0af6db9c8b0cfb
titadone <bloum93200@gmail.com>
2015-12-03 22:04:12 +0100

    re-organisation architecture

---
+++ include/head.php
@@ -0,0 +1,27 @@
+
+/**
+ * Created by PhpStorm.
+ * User: bloumy
+ * Date: 03/12/2015
+ * Time: 21:56
+ */
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+	<title>Nuit de l'info</title>
+    <link rel="stylesheet" href="./bootstrap/dist/css/bootstrap.min.css">
+	  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	  <script type="text/javascript" src="module.js"></script>
+	  <!-- Include all compiled plugins (below), or include individual files as needed -->
+	  <script src="./bootstrap/js/bootstrap.min.js"></script>
+
+      <link href="./module.css" rel="stylesheet">
+      <audio src="/audio/Pouvoir.mp3"></audio>
+
+  </head>
