461a025aa178e19a358ed6d7e4a6f5f3987c7e36
titadone <bloum93200@gmail.com>
2015-12-04 00:08:04 +0100

    deployment

--- include/head.php
+++ include/head.php
@@ -11,12 +11,12 @@
     <link rel="icon" href="favicon.ico">
 	<title>Nuit de l'info</title>
     <!--CSS-->
-    <link rel="stylesheet" href="./assets/style.css">
-    <link rel="stylesheet" href="./assets/bootstrap/dist/css/bootstrap.min.css">
+    <!--<link rel="stylesheet" href="./assets/style.css">-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <!--JAVASCRIPT-->
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script type="text/javascript" src="./assets/bootstrap/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="./assets/js/main.js"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+   <!-- <script type="text/javascript" src="./assets/js/main.js"></script>-->
     <!--AUDIO-->
     <audio src="/audio/Pouvoir.mp3"></audio>
   </head>
