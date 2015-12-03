b6732087553fa33b6bbbf3034ac787e11a0f3a15
MaximeAnseaume <anseaume.max@gmail.com>
2015-12-03 22:42:18 +0100

    changement API localisation

--- localisation.php
+++ localisation.php
@@ -2,23 +2,23 @@
 
 function get_client_ip() {
     $ipaddress = '';
-    if (getenv('HTTP_CLIENT_IP'))
-        $ipaddress = getenv('HTTP_CLIENT_IP');
-    else if(getenv('HTTP_X_FORWARDED_FOR'))
-        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-    else if(getenv('HTTP_X_FORWARDED'))
-        $ipaddress = getenv('HTTP_X_FORWARDED');
-    else if(getenv('HTTP_FORWARDED_FOR'))
-        $ipaddress = getenv('HTTP_FORWARDED_FOR');
-    else if(getenv('HTTP_FORWARDED'))
-       $ipaddress = getenv('HTTP_FORWARDED');
-    else if(getenv('REMOTE_ADDR'))
-        $ipaddress = getenv('REMOTE_ADDR');
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
     else
         $ipaddress = 'UNKNOWN';
     return $ipaddress;
 }
 
 $ip = get_client_ip();
-$details = json_decode(file_get_contents('http://freegeoip.net/json/'.$ip));
-echo 'IP : '.$details->ip.'<br>city : '.$details->city.'<br>latitude : '.$details->latitude.'<br> longitude : '.$details->longitude;
+$details = json_decode(file_get_contents('http://ipinfo.io/'.$ip.'/json'));
+echo 'IP : '.$details->ip.'<br>city : '.$details->city.'<br>lat / longitude : '.$details->loc;
