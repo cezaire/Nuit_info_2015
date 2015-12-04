b0b683cd431db617de0dcd8606aa16f4bde07fc1
jeremy <jeremy.zaire@hotmail.fr>
2015-12-04 01:52:03 +0100

    correction affichage index

--- include/head.php
+++ include/head.php
@@ -11,12 +11,10 @@
     <link rel="icon" href="favicon.ico">
 	<title>Nuit de l'info</title>
     <!--CSS-->
-    <!--<link rel="stylesheet" href="./assets/style.css">-->
+    <link rel="stylesheet" href="./assets/css/style.css" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <!--JAVASCRIPT-->
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
    <!-- <script type="text/javascript" src="./assets/js/main.js"></script>-->
-    <!--AUDIO-->
-    <audio src="/audio/Pouvoir.mp3"></audio>
   </head>
--- include/header.php
+++ include/header.php
@@ -1,8 +1,7 @@
 <header class="row" >
+
    <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top col-lg-12" role="navigation">
-
-       
 
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header" >
@@ -12,11 +11,11 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
-					<span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="index.php" id="logo" >
-					SafeLife
-				</a>
+                    SafeLife
+                </a>
 
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
@@ -28,72 +27,72 @@
                     <li>
                         <a href="#">Vidéos</a>
                     </li>
-				<?php
+                <?php
 
-					if(isset($_SESSION['id_user']) and isset($_SESSION['pseudo']) )
-					{
-						echo '<li>
-							<a href="contact.php">Nous contacter</a>
-						</li>';
-					}
+                    if(isset($_SESSION['id_user']) and isset($_SESSION['pseudo']) )
+                    {
+                        echo '<li>
+                            <a href="contact.php">Nous contacter</a>
+                        </li>';
+                    }
 
-				?>
+                ?>
 
                     <li>
                         <a href="apropos.php">A propos</a>
                     </li>
 
                 </ul>
-				<?php
+                <?php
 
 
 
-				if(isset($_SESSION['id_user']) and isset($_SESSION['pseudo']) )
-				{ ?>
-				  <ul class="nav navbar-nav navbar-right">
-					<li class="dropdown">
+                if(isset($_SESSION['id_user']) and isset($_SESSION['pseudo']) )
+                { ?>
+                  <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
 
-					  <a href="#" id="deconnexion-btn" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="glyphicon glyphicon-user"> </span> <?php echo $_SESSION['pseudo']  ?></a>
+                      <a href="#" id="deconnexion-btn" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="glyphicon glyphicon-user"> </span> <?php echo $_SESSION['pseudo']  ?></a>
 
-						  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-							<li role="presentation" > <a href="../index.php?action=deco"> <span class="glyphicon glyphicon-off" > </span> Se deconnecter </a>  </li>
-							<li role="presentation" > <a href=<?php echo '"/profile.php"' ?> > <span class="glyphicon glyphicon-user" > </span> Profil </a>  </li>
-							<li role="presentation" > <a href="http://projetphp.ismaeldiallo.fr/configurer_profil.php" > <span class="glyphicon glyphicon-cog" > </span> Configurer </a>  </li>
+                          <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+                            <li role="presentation" > <a href="../index.php?action=deco"> <span class="glyphicon glyphicon-off" > </span> Se deconnecter </a>  </li>
+                            <li role="presentation" > <a href=<?php echo '"/profile.php"' ?> > <span class="glyphicon glyphicon-user" > </span> Profil </a>  </li>
+                            <li role="presentation" > <a href="http://projetphp.ismaeldiallo.fr/configurer_profil.php" > <span class="glyphicon glyphicon-cog" > </span> Configurer </a>  </li>
 
-						  </ul>
-					</li>
-				  </ul>
+                          </ul>
+                    </li>
+                  </ul>
 
-				<?php }
-				else
-				{
-					echo ' <ul class="nav navbar-nav navbar-right">
-						<li class="dropdown">
+                <?php }
+                else
+                {
+                    echo ' <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown">
 
-						  <a href="#" id="connexion-btn" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="glyphicon glyphicon-off"> </span> Se connecter</a>
+                          <a href="#" id="connexion-btn" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="glyphicon glyphicon-off"> </span> Se connecter</a>
 
-							  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-								<li role="presentation" >
-								  <div id="user_connexion" >
-										<form method="POST" action="index.php">
-												<input type="text" id="speudo" name="pseudo" placeholder="Pseudo"/>
-												<input type="password" id="mdp" name="mdp" placeholder="Mot de passe"/><br/>
-												<input type="checkbox" id="remember" name="remember" checked="true"/>Sauvegarder<br/>
-												<input type="submit" id="connexion-bt" class="send-form-bt" name="connexion" value="Se connecter"/>
-										</form>
-										<a href="src/utilisateur/inscription.php" ><button id="inscription-bt" class="send-form-bt">S\'inscrire</button></a>
-								  </div>
-								</li>
-							  </ul>
-						</li>
-					  </ul>';
-				}
-				?>
+                              <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+                                <li role="presentation" >
+                                  <div id="user_connexion" >
+                                        <form method="POST" action="index.php">
+                                                <input type="text" id="speudo" name="pseudo" placeholder="Pseudo"/>
+                                                <input type="password" id="mdp" name="mdp" placeholder="Mot de passe"/><br/>
+                                                <input type="checkbox" id="remember" name="remember" checked="true"/>Sauvegarder<br/>
+                                                <input type="submit" id="connexion-bt" class="send-form-bt" name="connexion" value="Se connecter"/>
+                                        </form>
+                                        <a href="src/utilisateur/inscription.php" ><button id="inscription-bt" class="send-form-bt">S\'inscrire</button></a>
+                                  </div>
+                                </li>
+                              </ul>
+                        </li>
+                      </ul>';
+                }
+                ?>
             </div>
 
             <!-- /.navbar-collapse -->
 
- 
+
 
        <!-- /.container -->
     </nav>
